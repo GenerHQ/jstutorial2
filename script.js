1c97d0071ab39ac076f53647489145feaa5bae2f
GenerHQ <yambingpablitojrg@gmail.com>
2024-11-27 23:43:39 +0800

    jstutorial6

---
+++ script.js
@@ -0,0 +1,33 @@
+// let x = 10;
+// let y = "10";
+// alert(x == y);
+// alert(x === y);
+
+// let a = "Hello World";
+// let b = "hello World";
+// alert(a == b);
+
+// let x = 10;
+
+// if (x >= 18) {
+//   console.log("Legal Age");
+// } else {
+//   console.log("Not Legal Age");
+// }
+// alert("Thank you");
+
+let grade = 97.9;
+
+if (grade > 100) {
+  console.log("Invalid Grade");
+} else if (grade >= 98 && grade <= 100) {
+  console.log("With Highest Honor");
+} else if (grade >= 95 && grade < 98) {
+  console.log("With High Honor");
+} else if (grade >= 90 && grade < 95) {
+  console.log("With Honor");
+} else if (grade >= 75 && grade < 90) {
+  console.log("Passed");
+} else {
+  console.log("Failed");
+}
